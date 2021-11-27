aeaac1c4ac37475ed49f1af4b63825d4cf3d065d
Alex Briar <alexander.briar@bt.com>
2021-11-27 19:03:42 +0000

    Initial Commit

---
+++ ExchangeRate/ExchangeRateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeRateApp.swift
+//  ExchangeRate
+//
+//  Created by Alex on 27/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeRateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
